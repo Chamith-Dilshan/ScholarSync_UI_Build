9219785160d977bd2c72b44fe4d88c823186b754
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-07-18 13:49:29 +0530

    update button_icon.dart

--- lib/features/view/notifications_page.dart
+++ lib/features/view/notifications_page.dart
@@ -1,17 +1,19 @@
 
-
 // import 'package:flutter/material.dart';
+// import 'package:scholars_mobileapp/common/button_icon.dart';
+// import 'package:scholars_mobileapp/common/nav_bar.dart';
+// import 'package:scholars_mobileapp/constants/icon_constants.dart';
+// import 'package:scholars_mobileapp/theme/palette.dart';
 
 // class AuthField extends StatelessWidget {
 //   final TextEditingController controller;
-//   final notificationController = TextEditingController();
 //   final String hintText;
 //   final double fontSize;
 //   final bool? hideText;
 //   final String? frontIcon;
 //   final String? backIcon;
 
-//    AuthField({
+//    const AuthField({
 //     Key? key,
 //     required this.controller,
 //     required this.hintText,
@@ -20,6 +22,7 @@
 //     this.frontIcon,
 //     this.backIcon,
 //   }) : super(key: key);
+
 
 
 //   @override
@@ -37,26 +40,19 @@
 //   }
 // }
 
-// class NotificationsPage extends StatelessWidget {
+//   class NotificationsPage extends StatelessWidget {
 //   const NotificationsPage({Key? key}) : super(key: key);
-
-//   String getTimeAgo() {
-//     // Add your logic to calculate the time difference and return a formatted string
-//     // Example: return '20 minutes ago';
-//     // You can use packages like `timeago` to format the time difference
-//     return '20 minutes ago';
-//   }
 
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       backgroundColor: const Color(0xFFFBFBFB),
-//       appBar: AppBar(
+//         backgroundColor: const Color(0xFFFBFBFB),
+//         appBar: AppBar(
 //         backgroundColor: Colors.transparent,
 //         elevation: 0,
 //         title: const Text(
 //           'Notifications',
-//           style: TextStyle(
+//             style: TextStyle(
 //             fontFamily: 'Inter',
 //             fontSize: 22,
 //             fontWeight: FontWeight.w600,
@@ -64,158 +60,43 @@
 //           ),
 //         ),
 //         actions: [
-//           IconButton(
-//             color: const Color(0xFF181818),
-//             icon: const Icon(Icons.menu),
-//             onPressed: () {
-//               // Handle hamburger menu button press
-//             },
-//           ),
+//           ButtonIcon(
+//           onTap: () {}, 
+//           icon: IconConstants.hamburgerMenuIcon,
+//           backgroundColor: Palette.lightBackgroundColor,
+//           iconColor: Palette.darkGreenColor,
+//           size: 50.0,
+//         ),
 //         ],
 //       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             children: [
-//               const Align(
-//                 alignment: Alignment.topLeft,
-//                 child: Text(
-//                   'Today',
-//                   style: TextStyle(
-//                     color: Color(0xFF83898F),
-//                     fontSize: 12,
-//                   ),
-//                 ),
-//               ),
-
-
-
-
-//               const SizedBox(height: 10),
-//               Container(
-//                 width: 371,
-//                 height: 63.16,
-//                 alignment: Alignment.topLeft,
-//                 decoration: BoxDecoration(
-//                   color: Colors.white,
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//                 child: Row(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     const Padding(
-//                       padding: EdgeInsets.all(8.0),
-//                       child: Icon(
-//                         Icons.calendar_today,
-//                         color: Color.fromARGB(255, 5, 5, 5),
-//                       ),
-//                     ),
-//                     const SizedBox(width: 10),
-//                     Expanded(
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         mainAxisAlignment: MainAxisAlignment.start,
-//                         children: [
-//                           const Text(
-//                             'Sample notification message',
-//                             style: TextStyle(
-//                               color: Color(0xFF3A3F47),
-//                               fontSize: 11,
-//                             ),
-//                           ),
-//                           const SizedBox(height: 5),
-//                           Text(
-//                             getTimeAgo(),
-//                             style: const TextStyle(
-//                               color: Color(0xFF3A3F47),
-//                               fontSize: 11,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                     const Padding(
-//                       padding: EdgeInsets.all(8.0),
-//                       child: Icon(
-//                         Icons.close,
-//                         color: Color(0xFF83898F),
-//                         size: 17,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-
-
-
-
-//               const SizedBox(height: 10),
-//               // Add more notifications here
-              
-//             ],
-//           ),
-//         ),
+//       body: const SingleChildScrollView(
+  
 //       ),
 
-//       bottomNavigationBar: Container(
-//         color: const Color(0xFF132A13),
-//         padding: const EdgeInsets.symmetric(vertical: 8),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: [
-//             IconButton(
-//               icon: Stack(
-//                 children: [
-//                   Container(
-//                     width: 30,
-//                     height: 30,
-//                     decoration: const BoxDecoration(
-//                       shape: BoxShape.circle,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   const Positioned(
-//                     left: 5,
-//                     top: 5,
-//                     child: Icon(
-//                       Icons.add,
-//                       color: Color(0xFF132A13),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               onPressed: () {
-//                 // Handle Add button press
-//               },
-//             ),
-//             IconButton(
-//               icon: const Icon(Icons.home),
-//               onPressed: () {
-//                 // Handle Home button press
-//               },
-//             ),
-//             IconButton(
-//               icon: const Icon(Icons.calendar_today),
-//               onPressed: () {
-//                 // Handle Calendar button press
-//               },
-//             ),
-//             IconButton(
-//               icon: const Icon(Icons.notifications),
-//               onPressed: () {
-//                 // Handle Notifications button press
-//               },
-//             ),
-//             IconButton(
-//               icon: const Icon(Icons.person),
-//               onPressed: () {
-//                 // Handle My Profile button press
-//               },
-//             ),
-//           ],
+//   bottomNavigationBar: NavBar(
+//           onItemSelected: (int index) {
+//             // Handle navigation or actions based on the selected index
+//             // For example
+//           },
 //         ),
+//       );
+    
+//   }
+// }
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Notifications Page',
+//       theme: ThemeData(
+//         primarySwatch: Colors.green,
 //       ),
+//       home: const NotificationsPage(),
 //     );
 //   }
 // }
