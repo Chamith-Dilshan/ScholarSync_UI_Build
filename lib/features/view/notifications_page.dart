ebbeb811cb0efecf175040608602916b99b81e96
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-07-21 12:27:31 +0530

    update notifications_page

--- lib/features/view/notifications_page.dart
+++ lib/features/view/notifications_page.dart
@@ -1,102 +1,130 @@
 
-// import 'package:flutter/material.dart';
-// import 'package:scholars_mobileapp/common/button_icon.dart';
-// import 'package:scholars_mobileapp/common/nav_bar.dart';
-// import 'package:scholars_mobileapp/constants/icon_constants.dart';
-// import 'package:scholars_mobileapp/theme/palette.dart';
-
-// class AuthField extends StatelessWidget {
-//   final TextEditingController controller;
-//   final String hintText;
-//   final double fontSize;
-//   final bool? hideText;
-//   final String? frontIcon;
-//   final String? backIcon;
-
-//    const AuthField({
-//     Key? key,
-//     required this.controller,
-//     required this.hintText,
-//     required this.fontSize,
-//     this.hideText,
-//     this.frontIcon,
-//     this.backIcon,
-//   }) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:scholars_mobileapp/common/nav_bar.dart';
+import 'package:scholars_mobileapp/constants/icon_constants.dart';
+import 'package:scholars_mobileapp/features/view/login_page.dart';
+import 'package:scholars_mobileapp/theme/palette.dart';
+import 'package:scholars_mobileapp/common/custom_textfield.dart';
+import 'package:scholars_mobileapp/constants/ui_constants.dart';
+import 'package:scholars_mobileapp/theme/app_theme.dart';
+// import 'package:scholars_mobileapp/features/view/home_page.dart';
 
 
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return TextField(
-//       controller: controller,
-//       obscureText: hideText ?? false,
-//       style: TextStyle(fontSize: fontSize),
-//       decoration: InputDecoration(
-//         hintText: hintText,
-//         prefixIcon: frontIcon != null ? const Icon(Icons.ac_unit) : null,
-//         suffixIcon: backIcon != null ? const Icon(Icons.ac_unit) : null,
-//       ),
-//     );
-//   }
-// }
+class NotificationsPage extends StatelessWidget {
+const NotificationsPage({Key? key}) : super(key: key);
 
-//   class NotificationsPage extends StatelessWidget {
-//   const NotificationsPage({Key? key}) : super(key: key);
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: UIConstants.appBar(
+        titelCenter: false,
+        backIcon: IconConstants.hamburgerMenuIcon,
+        onBackIconButtonpressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const LogInPage()),
+          );
+        },
+      ),
+      
+   //body
+   body: Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: Column(children: [
+      const Align(alignment: Alignment.topCenter),
+//Notification 01
+      CustomTextField(firstLine: "First Line Text",
+          secondPartFirstline: "Second Part Text",
+          secondLine: "Second Line Text",
+          thirdLine: "Third Line Text",
+          firstLineStyle: const TextStyle(fontSize: 12, color: Colors.black),
+          secondLineStyle: const TextStyle(fontSize: 10, color: Colors.blue),
+          thirdLineStyle: const TextStyle(fontSize: 9, color: Colors.green),
+          secondPartFirstLineStyle: const TextStyle(fontWeight: FontWeight.bold),
+          controller: TextEditingController(),
+          ontapBox: () {
+            // onTap function for the Box
+          },
+          ontapFrontIcon: (){
+            //onTap funtion for FrontIcon
+          },
+          ontapBackIcon: () {
+            //ontap funtion for backIcon
+          },
+          frontIcon: IconConstants.bellFilledIcon,
+          backIcon: IconConstants.moonIcon,
+          frontIconScale: 40,
+          backIconScale: 40,
+          borderColor: Colors.transparent,
+          borderWidth: 0,
+          backgroundColor: Palette.lightBackgroundColor,
+          boxwidth: 400,
+          boxheight: 67,
+          padding: 16,  ),
+          const SizedBox(height: 10),
+//Notification 02
+  CustomTextField(firstLine: "First Line Text",
+          secondPartFirstline: "Second Part Text",
+          secondLine: "Second Line Text",
+          thirdLine: "Third Line Text",
+          firstLineStyle: const TextStyle(fontSize: 12, color: Colors.black),
+          secondLineStyle: const TextStyle(fontSize: 10, color: Colors.blue),
+          thirdLineStyle: const TextStyle(fontSize: 9, color: Colors.green),
+          secondPartFirstLineStyle: const TextStyle(fontWeight: FontWeight.bold),
+          controller: TextEditingController(),
+          ontapBox: () {
+            // onTap function for the Box
+          },
+          ontapFrontIcon: (){
+            //onTap funtion for FrontIcon
+          },
+          ontapBackIcon: () {
+            //ontap funtion for backIcon
+          },
+          frontIcon: IconConstants.bellFilledIcon,
+          backIcon: IconConstants.moonIcon,
+          frontIconScale: 40,
+          backIconScale: 40,
+          borderColor: Colors.transparent,
+          borderWidth: 0,
+          backgroundColor: Palette.lightBackgroundColor,
+          boxwidth: 400,
+          boxheight: 67,
+          padding: 16,  ),
+          const SizedBox(height: 10),
+    ],)
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: const Color(0xFFFBFBFB),
-//         appBar: AppBar(
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//         title: const Text(
-//           'Notifications',
-//             style: TextStyle(
-//             fontFamily: 'Inter',
-//             fontSize: 22,
-//             fontWeight: FontWeight.w600,
-//             color: Color(0xFF181818),
-//           ),
-//         ),
-//         actions: [
-//           ButtonIcon(
-//           onTap: () {}, 
-//           icon: IconConstants.hamburgerMenuIcon,
-//           backgroundColor: Palette.lightBackgroundColor,
-//           iconColor: Palette.darkGreenColor,
-//           size: 50.0,
-//         ),
-//         ],
-//       ),
-//       body: const SingleChildScrollView(
   
-//       ),
+   ),
 
-//   bottomNavigationBar: NavBar(
-//           onItemSelected: (int index) {
-//             // Handle navigation or actions based on the selected index
-//             // For example
-//           },
-//         ),
-//       );
+
+
+  bottomNavigationBar: NavBar(
+          onItemSelected: (int index) {
+            // Handle navigation or actions based on the selected index
+            // For example
+          },
+        ),
+      );
     
-//   }
-// }
+  }
+}
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(const MainApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Notifications Page',
-//       theme: ThemeData(
-//         primarySwatch: Colors.green,
-//       ),
-//       home: const NotificationsPage(),
-//     );
-//   }
-// }
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppThemeLight.theme,
+      debugShowCheckedModeBanner: false ,
+      home: const NotificationsPage(),
+    );
+  }
+}
