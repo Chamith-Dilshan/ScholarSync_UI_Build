22d0fe0143d5d3b715078ed600a4da52d8439e8e
Hasini Punsala <hasinipunsala@gmail.com>
2023-07-22 20:20:05 +0530

    Update kuppi_page.dart

--- lib/features/view/kuppi_page.dart
+++ lib/features/view/kuppi_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:scholars_mobileapp/common/search_bar.dart';
+import 'package:scholars_mobileapp/common/search_bar_2.dart';
 import 'package:scholars_mobileapp/constants/ui_constants.dart';
 import 'package:scholars_mobileapp/features/view/login_page.dart';
-import 'package:scholars_mobileapp/theme/palette.dart';
 
 import '../../constants/icon_constants.dart';
 
@@ -13,11 +12,10 @@ class KuppiPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: UIConstants.appBar(
-        title: 'Kuppi',
-        titleFontSize: 18,
-        titleFontWeight: FontWeight.w600,
-        titleCenter: true,
-        iconColor: Palette.lightGreenColor,
+        title: 'Kuppi Sessions',
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        titelCenter: true,
         frontIcon: IconConstants.leftArrowIcon,
         backIcon: IconConstants.hamburgerMenuIcon,
         frontIconToolTip: 'Back to login page',
@@ -34,7 +32,25 @@ class KuppiPage extends StatelessWidget {
           );
         },
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Call the CustomSearchBar here
+            CustomSearchBar(
+              hint: 'Search for kuppi sessions...',
+              onSearchSubmitted: (query) {
+                // Handle the search query here
+              },
+            ),
+            const SizedBox(
+                height:
+                    20), // Add some spacing if needed between the search bar and other content
+            const Text(
+                'Other content goes here'), // Add other content as needed
+          ],
+        ),
+      ),
     );
   }
 }
