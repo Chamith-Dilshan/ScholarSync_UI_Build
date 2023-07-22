1ce587974da5d6b4b4114b3f132a5c6a48957b85
Hansani Abeysekera <nwhsabeysekera@students.nsbm.ac.lk>
2023-07-22 19:12:57 +0530

    Update bottom navigation bar

--- lib/common/bottom_navigation_bar.dart
+++ lib/common/bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scholars_mobileapp/constants/icon_constants.dart';
 import '../theme/palette.dart';
 
 class MyBottomNavigationBar extends StatelessWidget {
@@ -23,11 +24,11 @@ class MyBottomNavigationBar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             //Have to add the icons of the bottom naviation bar and place them here according to the path
-            buildBottomNavItem(Icons.home, 'Home', 0),
+            buildBottomNavItem(IconConstants.homeIcon as IconData, 'Home', 0),
             buildBottomNavItem(Icons.calendar_today, 'Calendar', 1),
             buildAddBottomNavItem(),
-            buildBottomNavItem(Icons.notifications, 'Notification', 3),
-            buildBottomNavItem(Icons.person, 'Profile', 4),
+            buildBottomNavItem(IconConstants.notifOutlinedIcon as IconData, 'Notification', 3),
+            buildBottomNavItem(IconConstants.peopleIcon as IconData, 'Profile', 4),
           ],
         ),
       ),
@@ -83,7 +84,7 @@ class MyBottomNavigationBar extends StatelessWidget {
               ),
             ),
             Icon(
-              Icons.add,
+              IconConstants.addButtonIcon as IconData,
               color: selected ? PaletteLightMode.primaryGreenColor : PaletteLightMode.primaryGreenColor,
             ),
           ],
