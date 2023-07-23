8ea01c527f1b2a1abc7c1e046ba8dd002b377db0
Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>
2023-07-23 22:01:23 +0530

    Updae my profile page

--- lib/features/view/my_profile_page.dart
+++ lib/features/view/my_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scholars_mobileapp/common/text_container.dart';
 import 'package:scholars_mobileapp/constants/icon_constants.dart';
 import 'package:scholars_mobileapp/constants/ui_constants.dart';
 import 'package:scholars_mobileapp/common/bottom_navigation_bar.dart';
@@ -38,83 +39,71 @@ class _MyProfilePageState extends State<MyProfilePage> {
           );*/
         },
       ),
-    body: Column(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-         const Padding(
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
             child: ProfileInfo(),
           ),
           const SizedBox(height: 11),
-          Padding(
-            padding: const EdgeInsets.only(left: 25,right: 25),
+          const Padding(
+            padding: EdgeInsets.only(left: 25, right: 25),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
+                Text(
                   'My Projects',
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    // Handle "View All" click
-                  },
-                  child: const Text(
-                    'View All',
-                    style: TextStyle(
-                      color: PaletteLightMode.textColor,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
+                TextContainer(fontText: 'View All'),
               ],
             ),
           ),
           const SizedBox(height: 5),
           Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 18.0,
-                crossAxisSpacing: 25.0,
-                padding: const EdgeInsets.only(left: 25,right: 25),
-                  children: const [
-                      ProjectBox(
-                         projectNumber: '1',
-                         projectName: 'Project Name 1',
-                         date: '2023-07-23',
-                         githubLink: 'https://github.com/project1',
-                      ),
-                      ProjectBox(
-                         projectNumber: '2',
-                         projectName: 'Project Name 2',
-                         date: '2023-07-24',
-                         githubLink: 'https://github.com/project2',
-                      ),
-                      ProjectBox(
-                         projectNumber: '3',
-                         projectName: 'Project Name 3',
-                         date: '2023-07-25',
-                         githubLink: 'https://github.com/project3',
-                      ),
-                      ProjectBox(
-                        projectNumber: '4',
-                        projectName: 'Project Name 4',
-                        date: '2023-07-26',
-                       githubLink: 'https://github.com/project4',
-                      ),
-        ],
-        ),
-        ),
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 18.0,
+              crossAxisSpacing: 25.0,
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              children: const [
+                ProjectBox(
+                  projectNumber: '1',
+                  projectName: 'Project Name 1',
+                  date: '2023-07-23',
+                  githubLink: 'https://github.com/project1',
+                ),
+                ProjectBox(
+                  projectNumber: '2',
+                  projectName: 'Project Name 2',
+                  date: '2023-07-24',
+                  githubLink: 'https://github.com/project2',
+                ),
+                ProjectBox(
+                  projectNumber: '3',
+                  projectName: 'Project Name 3',
+                  date: '2023-07-25',
+                  githubLink: 'https://github.com/project3',
+                ),
+                ProjectBox(
+                  projectNumber: '4',
+                  projectName: 'Project Name 4',
+                  date: '2023-07-26',
+                  githubLink: 'https://github.com/project4',
+                ),
+              ],
+            ),
+          ),
         ],
       ),
-
-         bottomNavigationBar: MyBottomNavigationBar(
-           currentIndex: _currentIndex,
-           onTabTapped: _onTabTapped,
-        ),
+      bottomNavigationBar: MyBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTabTapped: _onTabTapped,
+      ),
     );
   }
 }
