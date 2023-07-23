7f6436d4330877b2e16a275cb03de7f55d129db4
Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>
2023-07-23 16:01:12 +0530

    Create academic_staff_page_tab.dart

---
+++ lib/features/widgets/academic_staff_page_tab.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:scholars_mobileapp/theme/palette.dart';
+
+class AcademicStaffPageTabs extends StatelessWidget {
+  final TabController tabController;
+
+  const AcademicStaffPageTabs({super.key, required this.tabController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: PaletteLightMode.transparentColor,
+      margin: const EdgeInsets.symmetric(vertical: 50),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TabBar(
+        controller: tabController,
+        labelColor: PaletteLightMode.titleColor,
+        // Set the color of the selected tab's text
+        unselectedLabelColor: PaletteLightMode.titleColor,
+        // Set the color of unselected tabs' text
+        indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: PaletteLightMode.secondaryGreenColor,
+          // Set the background color of the selected tab indicator
+        ),
+        indicatorSize: TabBarIndicatorSize.tab,
+        // Set the size of the indicator to match the tab's size
+        tabs: const [
+          Tab(text: 'DS'),
+          Tab(text: 'NS'),
+          Tab(text: 'CSSE'),
+          Tab(text: 'ISS'),
+        ],
+      ),
+    );
+  }
+}
