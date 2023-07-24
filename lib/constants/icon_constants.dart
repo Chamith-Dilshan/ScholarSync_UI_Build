fa0e77ced81e9bc5609e427b642070fa2e4b8531
Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>
2023-07-24 11:55:43 +0530

    Added images,icons and updated the relevant files

--- lib/constants/icon_constants.dart
+++ lib/constants/icon_constants.dart
@@ -30,4 +30,8 @@ class IconConstants {
   static const String graduationHatIcon = '$_svgPath/hat_graduation.svg';
   static const String addButtonIcon = '$_svgPath/add_button.svg';
   static const String personIcon = '$_svgPath/person.svg';
+  static const String profileIcon = '$_svgPath/profile.svg';
+  static const String horizontalDotsIcon = '$_svgPath/horizontal_dots.svg';
+  static const String cameraIcon = '$_svgPath/camera.svg';
+  static const String projectIcon = '$_svgPath/project_management.svg';
 }
--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -4,4 +4,15 @@ class ImageConstants {
   static const String logo = '$_imagepath/logo.png';
   static const String kuppi1 = '$_imagepath/MICROPYTHON.png';
   static const String img1 = '$_imagepath/img1.jpeg';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi3 = '$_imagepath/maths.png';
+  /*static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';
+  static const String kuppi2 = '$_imagepath/dbms.png';*/
 }
