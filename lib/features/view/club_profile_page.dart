b5ce57f666de071960a22bac56e991e47d89609b
Hasini Punsala <hasinipunsala@gmail.com>
2023-08-06 18:44:19 +0530

    Update club_profile_page.dart

--- lib/features/view/club_profile_page.dart
+++ lib/features/view/club_profile_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:scholars_mobileapp/common/circular_icon_button.dart';
+import 'package:scholars_mobileapp/features/widgets/circular_icon_button.dart';
 import 'package:scholars_mobileapp/constants/icon_constants.dart';
 import 'package:scholars_mobileapp/constants/ui_constants.dart';
 import 'package:scholars_mobileapp/constants/image_constants.dart';
 import 'package:scholars_mobileapp/theme/palette.dart';
+import '../../common/custom_elevated_button.dart';
+import '../../common/reusable_form_dialog.dart';
+import '../../common/text_form_field.dart';
+import '../widgets/club_text_container.dart';
+import '../widgets/custom_carousel.dart';
 
 class ClubProfilePage extends StatefulWidget {
   const ClubProfilePage({Key? key}) : super(key: key);
@@ -13,41 +18,112 @@ class ClubProfilePage extends StatefulWidget {
 }
 
 class _ClubProfilePageState extends State<ClubProfilePage> {
+  bool isOwner = false;
+  String clubName = 'AIESEC in NSBM';
+  String about =
+      'AIESEC is the world’s largest youth led organization advocating for youth leadership through global affairs. AIESEC in NSBM has over 90+ active global-minded and culturally sensitive members from across the island. As an Ofﬁcial Expansion, we have won the Most Outstanding Award and Most Progressive Expansion Award in 2018 & 2019, respectively.';
+  String masterInCharge = 'Ms. Gimhani Ranagalla\nMs. Dilki Hansika';
+  String president = 'Sachin Hettiarachchi';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: UIConstants.appBar(
-        title: 'AIESEC NSBM',
+        title: clubName,
         fontSize: 22,
         fontWeight: FontWeight.bold,
         titleCenter: false,
         backIcon: IconConstants.hamburgerMenuIcon,
-        onBackIconButtonpressed: () {
-          /* Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const LogInPage()),
-          );*/
-        },
+        onBackIconButtonpressed: () {},
       ),
-      body: Column(
-        children: [
-          _buildImageBannerWithCircularImage(),
-          const Text(
-            'AIESEC in NSBM',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-              color: PaletteLightMode.textColor,
-            ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              _buildImageBannerWithCircularImage(),
+              Text(
+                clubName,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: PaletteLightMode.textColor,
+                ),
+              ),
+              const SizedBox(height: 11),
+              if (isOwner)
+                CustomElevatedButton(
+                  label: 'Edit Profile',
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                  textSize: 10,
+                  onPressed: () {
+                    _showFormDialog(context);
+                  },
+                  height: 20,
+                  backgroundColor: PaletteLightMode.secondaryGreenColor,
+                ),
+              const SizedBox(height: 20),
+              CustomTextContainer(
+                heading: 'About',
+                text: about,
+                headingSize: 16.0,
+              ),
+              const SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: CustomTextContainer(
+                      heading: 'Master/Mistress in Charge',
+                      headingSize: 12.0,
+                      text: masterInCharge,
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: CustomTextContainer(
+                      heading: 'President',
+                      headingSize: 12.0,
+                      text: president,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Upcoming Events by $clubName',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: PaletteLightMode.textColor,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              CustomCarousel(
+                imageList: const [
+                  ImageConstants.aiesec1,
+                  ImageConstants.aiesec2,
+                  ImageConstants.aiesec3,
+                ],
+                containerPadding: 20.0,
+                showIconButton: isOwner ? true : false,
+                enableAutoScroll: isOwner ? false : true,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 
   Widget _buildImageBannerWithCircularImage() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 10),
       child: SizedBox(
         width: 370,
         height: 200,
@@ -68,16 +144,18 @@ class _ClubProfilePageState extends State<ClubProfilePage> {
     return Stack(
       children: [
         _buildImageBanner(),
-        const Positioned(
-          bottom: 10,
-          right: 10,
-          child: CircularIconButton(
-            buttonSize: 20,
-            icon: Icons.camera_alt,
-            iconColor: Colors.white,
-            buttonColor: PaletteLightMode.secondaryGreenColor,
+        if (isOwner)
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: CircularIconButton(
+              buttonSize: 20,
+              iconAsset: IconConstants.cameraIcon,
+              iconColor: PaletteLightMode.whiteColor,
+              buttonColor: PaletteLightMode.secondaryGreenColor,
+              onPressed: () {},
+            ),
           ),
-        ),
       ],
     );
   }
@@ -112,17 +190,65 @@ class _ClubProfilePageState extends State<ClubProfilePage> {
             ),
           ),
         ),
-        const Positioned(
-          bottom: -(buttonSize / 1000),
-          right: -(buttonSize / 1000),
-          child: CircularIconButton(
-            buttonSize: buttonSize,
-            icon: Icons.camera_alt,
-            iconColor: Colors.white,
-            buttonColor: PaletteLightMode.secondaryGreenColor,
+        if (isOwner)
+          Positioned(
+            bottom: -(buttonSize / 1000),
+            right: -(buttonSize / 1000),
+            child: CircularIconButton(
+              buttonSize: buttonSize,
+              iconAsset: IconConstants.cameraIcon,
+              iconColor: PaletteLightMode.whiteColor,
+              buttonColor: PaletteLightMode.secondaryGreenColor,
+              onPressed: () {},
+            ),
           ),
-        ),
       ],
     );
   }
+}
+
+void _showFormDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return ReusableFormDialog(
+        title: 'Edit Profile',
+        buttonLabel: 'Save',
+        formFields: [
+          ReusableTextField(
+            labelText: 'About',
+            isMultiline: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please tell us about the club';
+              }
+              return null;
+            },
+            onSaved: (value) {},
+          ),
+          ReusableTextField(
+            labelText: 'Master/Mistress in Charge',
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please add master/mistress in charge';
+              }
+              return null;
+            },
+            onSaved: (value) {},
+          ),
+          ReusableTextField(
+            labelText: 'President',
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the name of the president';
+              }
+              return null;
+            },
+            onSaved: (value) {},
+          ),
+        ],
+        onSubmit: (formData) {},
+      );
+    },
+  );
 }
