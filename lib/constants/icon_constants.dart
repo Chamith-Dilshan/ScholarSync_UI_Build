2abd8981e5b6f4c084d7573636a665cddf0630a3
Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>
2023-08-15 16:17:35 +0530

    Added few icons, updated icon_constants file

--- lib/constants/icon_constants.dart
+++ lib/constants/icon_constants.dart
@@ -33,4 +33,8 @@ class IconConstants {
   static const String closeIcon = '$_svgPath/close.svg';
   static const String cameraIcon = '$_svgPath/camera.svg';
   static const String deleteIcon = '$_svgPath/delete.svg';
+  static const String horizontalDotsIcon = '$_svgPath/horizontal_dots.svg';
+  static const String profileIcon = '$_svgPath/profile.svg';
+  static const String projectManagementIcon =
+      '$_svgPath/project_management.svg';
 }
