19dbb1076c9c224dc04855799f968b00f7e47776
Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>
2023-09-06 16:57:06 +0530

    Added more images, Updated some files

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -10,4 +10,20 @@ class ImageConstants {
   static const String aiesec2 = '$_imagepath/aiesec2.jpeg';
   static const String aiesec3 = '$_imagepath/aiesec3.jpeg';
   static const String profileImage = '$_imagepath/profileImg.png"';
+  static const String clubIcon1 = '$_imagepath/Club1_cogni.png';
+  static const String clubIcon2 = '$_imagepath/Club2_DevApp.png';
+  static const String clubIcon3 = '$_imagepath/Club3_HackShield.png';
+  static const String clubIcon4 = '$_imagepath/Club4_InnovaTech.png';
+  static const String clubIcon5 = '$_imagepath/Club5_Codex.png';
+  static const String clubIcon6 = '$_imagepath/Club6_GamersGuide.png';
+  static const String kuppiImg1 = '$_imagepath/Kuppi_maths.png';
+  static const String kuppiImg2 = '$_imagepath/Kuppi_dbms.png';
+  static const String kuppiImg3 = '$_imagepath/Kuppi_web.png';
+  static const String eventImg1 = '$_imagepath/event1_hackathon.png';
+  static const String eventImg2 = '$_imagepath/event2_webinar.png';
+  static const String eventImg3 = '$_imagepath/event3_charity.png';
+  static const String eventImg4 = '$_imagepath/event4_gaming.png';
+  static const String eventImg5 = '$_imagepath/event5_music.png';
+  static const String eventImg6 = '$_imagepath/event6_beachcleanup.png';
+  static const String eventImg7 = '$_imagepath/event7_mobileappdev.png';
 }
